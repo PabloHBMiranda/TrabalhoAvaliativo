5e580a04e021a172b3466db402de257e0d0cfa9b
Pablo Miranda <112266050+PabloHBMiranda@users.noreply.github.com>
2022-09-21 20:38:55 -0300

    "Atualização do Meu Trabalho 21/09/2022"

--- app/src/main/java/com/example/trabalhoavaliativo1/CodigoSecreto.java
+++ app/src/main/java/com/example/trabalhoavaliativo1/CodigoSecreto.java
@@ -3,6 +3,7 @@ package com.example.trabalhoavaliativo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,14 +15,54 @@ import java.util.Random;
 
 public class CodigoSecreto extends AppCompatActivity {
 
-    public void SetarTexto(){
-
+    public void SetarTexto(String l, String x1, String x2, String x3, String x4, int i,TextView txt){
+        txt.setText(l);
+        if(l.equals(x1)||l.equals(x2)||l.equals(x3)||l.equals(x4)){
+            if(i == 0){
+                if(l.equals(x1)){
+                    txt.setTextColor(Color.GREEN);
+                }
+                else{
+                    txt.setTextColor(Color.YELLOW);
+                }
+            }
+            else{
+                if(i == 1){
+                    if(l.equals(x2)){
+                        txt.setTextColor(Color.GREEN);
+                    }
+                    else{
+                        txt.setTextColor(Color.YELLOW);
+                    }
+                }
+                else{
+                    if(i==2){
+                        if(l.equals(x3)){
+                            txt.setTextColor(Color.GREEN);
+                        }
+                        else{
+                            txt.setTextColor(Color.YELLOW);
+                        }
+                    }
+                    else{
+                        if(l.equals(x4)){
+                            txt.setTextColor(Color.GREEN);
+                        }
+                        else{
+                            txt.setTextColor(Color.YELLOW);
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            txt.setTextColor(Color.RED);
+        }
     }
 
     Button CDSCRTbtn1, CDSCRTbtn2;
     EditText CDSCRTLYed1, CDSCRTLYed2, CDSCRTLYed3, CDSCRTLYed4;
     TextView CDSCRTL2txt1, CDSCRTL2txt2, CDSCRTL2txt3, CDSCRTL2txt4;
-    String resultado = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,27 +80,35 @@ public class CodigoSecreto extends AppCompatActivity {
 
         String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random r = new Random();
+        String x1="A",x2="B",x3="C",x4="D";
+        String resultado = "ABCD";
         //TextView texto;
         //texto.setTextColor(Color.);
-        for(int i = 0; i < 4; i++){
-            int x = r.nextInt(26);
-            resultado += letras.charAt(x);
-        }
+       /* int x = r.nextInt(26);
+        x1 = letras.charAt(x);
+        x = r.nextInt(26);
+        x2 = letras.charAt(x);
+        x = r.nextInt(26);
+        x3 = letras.charAt(x);
+        x = r.nextInt(26);
+        x4 = letras.charAt(x);*/
        CDSCRTbtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!(CDSCRTLYed1.getText().equals("")) && !(CDSCRTLYed2.getText().equals("")) && !(CDSCRTLYed3.getText().equals(""))
-                && !(CDSCRTLYed4.getText().equals(""))){
+                if(!CDSCRTLYed1.getText().toString().isEmpty() && !CDSCRTLYed2.getText().toString().isEmpty() && !CDSCRTLYed3.getText().toString().isEmpty()
+                && !CDSCRTLYed4.getText().toString().isEmpty()){
                     String ED1, ED2, ED3, ED4;
                     ED1 = CDSCRTLYed1.getText().toString().toUpperCase();
                     ED2 = CDSCRTLYed2.getText().toString().toUpperCase();
                     ED3 = CDSCRTLYed3.getText().toString().toUpperCase();
                     ED4 = CDSCRTLYed4.getText().toString().toUpperCase();
-                    if(ED1.equals(resultado.charAt(0))){
-                    }
+                    SetarTexto(ED1,x1, x2,x3,x4,0,CDSCRTL2txt1);
+                    SetarTexto(ED2,x1, x2,x3,x4,1,CDSCRTL2txt2);
+                    SetarTexto(ED3,x1, x2,x3,x4,2,CDSCRTL2txt3);
+                    SetarTexto(ED4,x1, x2,x3,x4,3,CDSCRTL2txt4);
                 }
                 else{
-                    Toast.makeText(CodigoSecreto.this, "Preencha todos os campos", Toast.LENGTH_LONG);
+                    Toast.makeText(CodigoSecreto.this, "Preencha todos os campos", Toast.LENGTH_LONG).show();
                 }
             }
         });
